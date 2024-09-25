2ff406b58e9f8c81207f9a08cb03430d9c77665e
EmixamL <emixamlaferriere@gmail.com>
2024-09-25 13:12:13 -0400

    removed useless line

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -64,7 +64,6 @@ using (var scope = app.Services.CreateScope())
 
     try
     {
-        string url = "https://data.montreal.ca/dataset/4604afb7-a7c4-4626-a3ca-e136158133f2/resource/cbdca706-569e-4b4a-805d-9af73af03b14/download/piscines.csv";
         var dataController = services.GetRequiredService<DataController>();
         var urls = builder.Configuration.GetSection("SourceUrls").Get<Dictionary<string, string>>() ?? new Dictionary<string, string>();
         string firstUrl = urls["piscines"];
